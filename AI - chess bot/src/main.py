19ef3ad20a21994950f47448f10a243b75834ebd
ZaedenDaniel <114672661+ZaedenDaniel@users.noreply.github.com>
2024-06-15 08:25:59 +0530

    Enabled Dragging

--- AI - chess bot/src/main.py
+++ AI - chess bot/src/main.py
@@ -11,18 +11,46 @@ class Main:
     pygame.display.set_caption("Chess")
     self.game = Game()
 
+
   def mainloop(self):
     screen = self.screen
     game = self.game
+    dragger = self.game.dragger
+    board = self.game.board
 
     while True:
-
-      
       game.show_bg(screen)
       game.show_pieces(screen)
+
+      if dragger.dragging:
+        dragger.update_blit(screen)
        
       for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+          dragger.update_mouse(event.pos)
+
+          clicked_row = dragger.mouseY // SQSIZE
+          clicked_col = dragger.mouseX // SQSIZE
+          #incase piece exists at the clicked square
+          if board.squares[clicked_row][clicked_col].has_piece():
+            piece = board.squares[clicked_row][clicked_col].piece
+            dragger.save_initial(event.pos)
+            dragger.drag_piece(piece)
+        
+        #moving selected piece with the mouse
+        elif event.type == pygame.MOUSEMOTION:
+          if dragger.dragging:
+            dragger.update_mouse(event.pos)
+            game.show_bg(screen)
+            game.show_pieces(screen)
+            dragger.update_blit(screen)
+        #releasing the piece at a given square
+        elif event.type == pygame.MOUSEBUTTONUP:
+          dragger.undrag_piece()
+        
+        #quit game
+        elif event.type == pygame.QUIT:
           pygame.quit()
           sys.exit()
       pygame.display.update()
